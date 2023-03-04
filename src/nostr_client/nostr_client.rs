0717829d188acfb313d18bcd1a3671714c1bdc71
tee8z <tee8z@protonmail.com>
2023-03-04 00:03:31 -0500

    getting close to a working setup, need to wire up the long running tasks for the two clients

--- src/nostr_client/nostr_client.rs
+++ src/nostr_client/nostr_client.rs
@@ -1,9 +1,10 @@
-use crate::bot::Message;
+use crate::bot::{Channels, Message};
 use crate::{configuration::NostrSettings, error_fmt::error_chain_fmt};
 use nostr_sdk::prelude::*;
 use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
 use std::fmt::Debug;
+use std::future::{IntoFuture, Ready, ready};
 use std::str::FromStr;
 
 #[derive(thiserror::Error)]
@@ -11,7 +12,7 @@ pub enum NostrError {
     #[error("Failed to send request")]
     FailedToSend,
     #[error("Failed to validate pubkey")]
-    FailedPubkeyValidation
+    FailedPubkeyValidation,
 }
 
 impl Debug for NostrError {
@@ -20,17 +21,21 @@ impl Debug for NostrError {
     }
 }
 
-
 pub struct NostrClient {
     private_key: Secret<String>,
     public_key: String,
     client: Client,
     listen_relays: Vec<String>,
     db_pool: PgPool,
+    mempool_comm: Channels,
 }
 
 impl NostrClient {
-    pub async fn build(configuration: NostrSettings, db: PgPool) -> Result<Self, anyhow::Error> {
+    pub async fn build(
+        configuration: NostrSettings,
+        db: PgPool,
+        mempool_comm: Channels,
+    ) -> Result<Self, anyhow::Error> {
         let private_key = Secret::new(configuration.private_key.expose_secret().to_owned());
         let listen_relays = configuration.nostr_relays;
         let bot_keys = Keys::from_sk_str(private_key.expose_secret()).unwrap();
@@ -54,6 +59,7 @@ impl NostrClient {
             client: client,
             listen_relays: listen_relays,
             db_pool: db,
+            mempool_comm: mempool_comm,
         })
     }
 
@@ -62,7 +68,6 @@ impl NostrClient {
         client_pk: &str,
         msg: Message,
     ) -> Result<(), NostrError> {
-
         let pubkey = XOnlyPublicKey::from_str(client_pk.into())
             .map_err(|_| NostrError::FailedPubkeyValidation)?;
 
@@ -77,11 +82,22 @@ impl NostrClient {
         let mut notifcations = self.client.notifications();
         while let Ok(notifcation) = notifcations.recv().await {
             println!("{notifcation:?}");
-            
+
             /*TODO:
              * Add to the DB the user's alert request, use their pubkey to assoicate the alert
              * Send a response back to the user who requested the alert to confirm it was recieved
              */
         }
+    }
+}
+
+
+impl IntoFuture for NostrClient {
+    type Output = Result<(), std::io::Error>;
+    type IntoFuture = Ready<Self::Output>;
+
+    fn into_future(self) -> Self::IntoFuture {
+        //TODO: call the forever loop here to continue listen to our configured list of relays
+        ready(Ok(()))
     }
 }
